c9155688f26d4a9e8cd7ba2a2b19c1fabc283101
Nhlalo <nhlalonkosi@gmail.com>
2026-04-21 12:41:59 +0200

    feat: create authentication middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import logger from "../utils/logger.js";
+
+const SECRET_KEY = process.env.JWT_SECRET || "my-secret-key";
+
+export default function authenticateToken(req, res, next) {
+  const token = req.cookies.accessToken;
+
+  if (!token) {
+    return res.status(401).json({ error: "Access denied. No token provided." });
+  }
+
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ error: "Invalid or expired token" });
+    }
+
+    req.user = decoded;
+    next();
+  });
+}
