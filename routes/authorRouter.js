37694369a0c6cf3825d1c0d904dbd8aa0e9a22f3
Nhlalo <nhlalonkosi@gmail.com>
2026-04-21 12:45:16 +0200

    feat: add login, refresh and logout route paths

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,13 +1,21 @@
 import express from "express";
-import { login, setPassword } from "../controller/authController.js";
+import {
+  login,
+  setPassword,
+  logout,
+  generateNewToken,
+} from "../controller/authController.js";
 import {
   loginValidationChain,
   passwordValidationChain,
 } from "../validators/authValidators.js";
+import authenticateToken from "../middleware/authMiddleware.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/login", loginValidationChain, login);
 authRouter.post("/set-password", passwordValidationChain, setPassword);
+authRouter.post("/refresh", generateNewToken);
+authRouter.post("/login", loginValidationChain, login);
+authRouter.post("/logout", authenticateToken, logout);
 
 export { authRouter };
