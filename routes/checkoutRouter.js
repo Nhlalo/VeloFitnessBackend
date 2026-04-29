ba7776b784add66807fc1dbaaaa59502e4532b9e
Nhlalo <nhlalonkosi@gmail.com>
2026-04-29 13:59:34 +0200

    refactor: remove unnecessary imports within checkout

--- routes/checkoutRouter.js
+++ routes/checkoutRouter.js
@@ -1,9 +1,8 @@
 import express from "express";
 import { registerValidationChain } from "../validators/checkoutValidators.js";
 import { checkout } from "../controller/checkoutController.js";
-import { createUserProfile } from "../controller/createUserProfileController.js";
 
 const checkoutRouter = express.Router();
-checkoutRouter.post("/", registerValidationChain, checkout, createUserProfile);
+checkoutRouter.post("/", registerValidationChain, checkout);
 
 export { checkoutRouter };
