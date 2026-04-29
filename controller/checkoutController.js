37754f43502b234d99c3acc283f98b8598cfdf58
Nhlalo <nhlalonkosi@gmail.com>
2026-04-29 13:58:20 +0200

    refactor: shorten the code

--- controller/checkoutController.js
+++ controller/checkoutController.js
@@ -5,32 +5,35 @@ import { membershipPrices } from "../data/membershipPrice.js";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const checkout = async (req, res, next) => {
+  const { email, membershipTitle } = req.validatedData;
+  logger.info({ email }, `Process payment commence`);
+
+  const lowercaseMembershipTitle = membershipTitle.toLowerCase();
+  const price = membershipPrices[lowercaseMembershipTitle];
+
+  if (!price || typeof price !== "number" || isNaN(price)) {
+    logger.error({ err: error, email }, "Invalid membership price");
+
+    return res.status(400).json({
+      error: "Invalid membership price",
+    });
+  }
+
   try {
-    const { email, userClub } = req.validatedData;
-    logger.info({ email }, `Process payment commence`);
-
-    const lowercaseUserClub = userClub.toLowerCase();
-    const price = membershipPrices[lowercaseUserClub];
-
-    if (!price || typeof price !== "number" || isNaN(price)) {
-      return res.status(400).json({
-        error: "Invalid membership price",
-      });
-    }
     const paymentIntent = await stripe.paymentIntents.create({
       amount: Math.round(price * 100),
       currency: "usd",
       metadata: {
-        product_name: userClub,
+        product_name: membershipTitle,
         timestamp: new Date().toISOString(),
       },
       payment_method_types: ["card"],
     });
 
-    // const confirmedPayment = await stripe.paymentIntents.confirm(
-    //   paymentIntent.id,
-    //   { payment_method: "pm_card_visa" },
-    // );
+    const confirmedPayment = await stripe.paymentIntents.confirm(
+      paymentIntent.id,
+      { payment_method: "pm_card_visa" },
+    );
 
     req.paymentIntentId = paymentIntent.id;
     res.json({
@@ -38,6 +41,7 @@ const checkout = async (req, res, next) => {
       paymentIntentId: paymentIntent.id,
     });
   } catch (error) {
+    logger.error({ err: error, email }, "failed to complete payment");
     next(error);
   }
 };
