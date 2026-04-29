78533e8b19fef135fcb6b37620ffb12df2bed883
Nhlalo <nhlalonkosi@gmail.com>
2026-04-29 13:54:29 +0200

    feat: use token validation within authentication

--- validators/authValidators.js
+++ validators/authValidators.js
@@ -4,23 +4,62 @@ import {
   validateEmail,
   validatePassword,
   validateConfirmPassword,
+  validateToken,
 } from "./userValidators.js";
-import { sanitizePassword } from "./userSanitizers.js";
+import {
+  sanitizePassword,
+  sanitizeEmail,
+  sanitizeToken,
+} from "./userSanitizers.js";
 import { dataValidation } from "../middleware/validationMiddleware.js";
 
 const loginValidationChain = [
   validateEmail(),
   validatePassword(),
+  sanitizeEmail(),
   sanitizePassword("password"),
   dataValidation("Validation failed for signup", "Log in validation passed"),
 ];
 const passwordValidationChain = [
+  validateToken(),
   validateEmail(),
   validatePassword(),
   validateConfirmPassword(),
+  sanitizeToken(),
+  sanitizeEmail(),
   sanitizePassword("password"),
   sanitizePassword("confirmPassword"),
   dataValidation("Password creation failed", "Password set successfully"),
 ];
 
-export { loginValidationChain, passwordValidationChain };
+const resetPasswordRequestValidationChain = [
+  validateEmail(),
+  sanitizeEmail(),
+  dataValidation("Password reset failed", "Password set successfully"),
+];
+const resetPasswordValidationChain = [
+  validateToken(),
+  validatePassword(),
+  validateConfirmPassword(),
+  sanitizeToken(),
+  sanitizePassword("password"),
+  sanitizePassword("confirmPassword"),
+
+  dataValidation("Password creation failed", "Password set successfully"),
+];
+const newAcessTokenValidationChain = [
+  validateEmail(),
+  sanitizeEmail(),
+  dataValidation(
+    "Failed to generate new access token",
+    "Access token generated successfully",
+  ),
+];
+
+export {
+  loginValidationChain,
+  passwordValidationChain,
+  resetPasswordRequestValidationChain,
+  resetPasswordValidationChain,
+  newAcessTokenValidationChain,
+};
