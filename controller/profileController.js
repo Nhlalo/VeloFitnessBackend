0382fe7e4ba708edfd0eb172493025544878f618
Nhlalo <nhlalonkosi@gmail.com>
2026-05-18 21:59:01 +0200

    feat: add a controller to update user information

--- controller/profileController.js
+++ controller/profileController.js
@@ -4,6 +4,8 @@ import logger from "../utils/logger.js";
 import crypto from "crypto";
 import userModel from "../model/userModels.js";
 import { membershipPrices } from "../data/membershipPrice.js";
+import { extractPaymentDetails } from "../utils/extractPaymentDetails.js";
+import { createPaymentConfirmationEmail } from "../utils/emailTemplate.js";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
@@ -23,21 +25,6 @@ const createUserProfile = async (req, res, next) => {
   logger.info(`${email}: profile creation started`);
 
   try {
-    // Verify payment actually succeeded
-    const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
-
-    if (paymentIntent.status !== "succeeded") {
-      logger.warn(
-        `${email}: Payment not successful - Status: ${paymentIntent.status}`,
-      );
-      return res.status(400).json({
-        success: false,
-        error: "Payment not successful",
-        message: `Cannot create account. Payment status: ${paymentIntent.status}`,
-        paymentStatus: paymentIntent.status,
-      });
-    }
-
     const user = await userModel.findByEmail(email);
 
     if (user) {
@@ -51,6 +38,68 @@ const createUserProfile = async (req, res, next) => {
           "An account with this email already exists. Please log in instead.",
       });
     }
+
+    // Verify payment actually succeeded
+    const paymentIntent = await stripe.paymentIntents.retrieve(
+      paymentIntentId,
+      {
+        expand: ["payment_method", "charges.data.balance_transaction"],
+      },
+    );
+
+    if (paymentIntent.status !== "succeeded") {
+      logger.warn(
+        `${email}: Payment not successful - Status: ${paymentIntent.status}`,
+      );
+      return res.status(400).json({
+        success: false,
+        error: "Payment not successful",
+        message: `Cannot create account. Payment status: ${paymentIntent.status}`,
+        paymentStatus: paymentIntent.status,
+      });
+    }
+
+    const paymentDetails = await extractPaymentDetails(paymentIntent);
+
+    const paymentConfirmationEmailData = {
+      customerName: name + " " + surname,
+      amount: paymentDetails.formattedAmount,
+      paymentDate: paymentDetails.paymentDate,
+      paymentMethod: paymentDetails.paymentMethod,
+      transactionId: paymentDetails.transactionId,
+      membershipType: membershipTitle,
+      billingPeriod: "Monthly",
+      nextPaymentDate: paymentDetails.nextPaymentDate,
+      invoiceNumber: paymentDetails.invoiceNumber,
+    };
+    const response = await fetch("https://api.resend.com/emails", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        from: "onboarding@resend.dev",
+        to: email,
+        subject: "Payment Confirmation",
+        html: createPaymentConfirmationEmail(paymentConfirmationEmailData),
+      }),
+    });
+
+    if (!response.ok) {
+      if (response.status === 401) {
+        logger.error({ email }, "Invalid or missing Resend API key");
+        throw new Error("Invalid or missing Resend API key");
+      }
+      if (response.status === 429) {
+        logger.error({ email }, "Rate limited by Resend");
+        throw new Error("Rate limited by Resend");
+      }
+      logger.error({ email }, "Resend Server down");
+      throw new Error("Resend Server down");
+    }
+
+    logger.info({ email }, "Email sent successfully");
 
     const newUser = await userModel.createAccount(
       name,
@@ -91,5 +140,46 @@ const createUserProfile = async (req, res, next) => {
     next(error);
   }
 };
+const updatePersonalDetails = async (req, res, next) => {
+  const { name, surname, email, zipCode, phoneNumber } = req.validatedData;
 
-export { createUserProfile };
+  logger.info(`${email}:  "Initiating personal details change process"`);
+
+  try {
+    const updatedUser = await userModel.updatePersonalDetails(
+      email,
+      name,
+      surname,
+      zipCode,
+      phoneNumber,
+    );
+
+    if (!updatedUser) {
+      logger.warn(
+        { email },
+        "User not found or personal details update failed",
+      );
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    logger.info({ email }, "personal details change successful");
+
+    res.status(200).json({
+      success: true,
+      message: "Personal details changed successfully",
+      data: {
+        success: true,
+        email: updatedUser.email,
+        changedAt: new Date().toISOString(),
+      },
+    });
+  } catch (error) {
+    logger.error({ err: error, email }, "Personal details change failed");
+    next(error);
+  }
+};
+
+export { createUserProfile, updatePersonalDetails };
