2e21f20f20be9846d167775331d4847fa07ea2d7
Nhlalo <nhlalonkosi@gmail.com>
2026-05-18 22:07:10 +0200

    feat: implement route to update user's personal details

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -1,8 +1,18 @@
 import express from "express";
 import { registerValidationChain } from "../validators/createUserProfileValidators.js";
-import { createUserProfile } from "../controller/profileController.js";
+import { updatePersonalDetailsValidationChain } from "../validators/profileValidators.js";
+import {
+  createUserProfile,
+  updatePersonalDetails,
+} from "../controller/profileController.js";
+import authenticateToken from "../middleware/authMiddleware.js";
 
 const profileRouter = express.Router();
 
 profileRouter.post("/create", registerValidationChain, createUserProfile);
+profileRouter.post(
+  "/update",
+  updatePersonalDetailsValidationChain,
+  updatePersonalDetails,
+);
 export { profileRouter };
