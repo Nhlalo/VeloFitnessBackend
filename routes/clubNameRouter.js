72749628c943b582c8826e133d1c90572fc90edd
Nhlalo <nhlalonkosi@gmail.com>
2026-05-18 22:06:35 +0200

    fix: add the token authentication controller and data validation function to the clubNameRouter

--- routes/clubNameRouter.js
+++ routes/clubNameRouter.js
@@ -2,14 +2,18 @@ import express from "express";
 import { validateClubName } from "../validators/userValidators.js";
 import { sanitizeClubName } from "../validators/userSanitizers.js";
 import { changeClubName } from "../controller/clubNameController.js";
+import { dataValidation } from "../middleware/validationMiddleware.js";
+import authenticateToken from "../middleware/authMiddleware.js";
 
 const clubNameRouter = express.Router();
 
 //For club name change
 clubNameRouter.patch(
   "/",
+  authenticateToken,
   validateClubName(),
   sanitizeClubName(),
+  dataValidation("Club name change failed", "Club name change successful"),
   changeClubName,
 );
 
